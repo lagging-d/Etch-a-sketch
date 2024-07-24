16f9e54180a0b953f63d64e47465bd18c4c5ede3
Nikiro-Ja <143513350+Nikiro-Ja@users.noreply.github.com>
2024-07-24 15:41:47 +0300

    Add another 15 squares to complete the grid

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const gridContainer = document.querySelector(`.gridContainer`);
 const gridSquare = document.createElement(`div`);
 gridSquare.classList.add(`square`);
 gridContainer.appendChild(gridSquare);
+
+for (let i = 1; i < 16; ++i) {
+    let anotherSquare = gridSquare.cloneNode(false);
+    gridContainer.appendChild(anotherSquare);
+}
