8e3981904b4c711db3b1e14a5ff4d966c36e7e42
Nikiro-Ja <143513350+Nikiro-Ja@users.noreply.github.com>
2024-07-24 15:26:38 +0300

    Append the first square to container

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const gridContainer = document.querySelector(`.gridContainer`);
 
 const gridSquare = document.createElement(`div`);
 gridSquare.classList.add(`square`);
+gridContainer.appendChild(gridSquare);
