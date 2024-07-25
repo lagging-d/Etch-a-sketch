77e786b685bf4b73d3901587c87d0f005d481b9a
Nikiro-Ja <143513350+Nikiro-Ja@users.noreply.github.com>
2024-07-25 07:33:43 +0300

    Add logic for blacking out the squares

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ for (let i = 1; i < 16; ++i) {
     let anotherSquare = gridSquare.cloneNode(false);
     gridContainer.appendChild(anotherSquare);
 }
+
+const square = document.querySelectorAll(`.square`);
+
+let squareArr = Array.from(square);
+
+squareArr.forEach( (elem) => {
+    elem.addEventListener(`mouseenter`, () => {
+        elem.setAttribute(`style`, `background-color: black;`);
+
+    
+    })
+})
+
